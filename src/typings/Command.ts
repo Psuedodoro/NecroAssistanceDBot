40c60bfa4320b614061fae8e7e68262f2fce94a8
Psuedodoro <bearcodes@outlook.com>
2022-04-18 14:19:21 +0100

    🗿

--- src/typings/Command.ts
+++ src/typings/Command.ts
@@ -1,34 +1,25 @@
 import {
-    ChatInputApplicationCommandData,
-    CommandInteraction,
-    CommandInteractionOptionResolver,
-    GuildMember,
-    PermissionResolvable
+	ChatInputApplicationCommandData,
+	CommandInteraction,
+	CommandInteractionOptionResolver,
+	GuildMember,
+	PermissionResolvable,
 } from "discord.js";
 import { ExtendedClient } from "../structures/Client";
 
-/**
- * {
- *  name: "commandname",
- * description: "any description",
- * run: async({ interaction }) => {
- *
- * }
- * }
- */
 export interface ExtendedInteraction extends CommandInteraction {
-    member: GuildMember;
+	member: GuildMember;
 }
 
 interface RunOptions {
-    client: ExtendedClient;
-    interaction: ExtendedInteraction;
-    args: CommandInteractionOptionResolver;
+	client: ExtendedClient;
+	interaction: ExtendedInteraction;
+	args: CommandInteractionOptionResolver;
 }
 
 type RunFunction = (options: RunOptions) => any;
 
 export type CommandType = {
-    userPermissions?: PermissionResolvable[];
-    run: RunFunction;
+	userPermissions?: PermissionResolvable[];
+	run: RunFunction;
 } & ChatInputApplicationCommandData;
