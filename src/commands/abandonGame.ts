68406a08c631d61af03e1e93d1abf7664b0ee698
Psuedodoro <bearcodes@outlook.com>
2022-04-25 22:22:50 +0100

    Bug fixes

--- src/commands/abandonGame.ts
+++ src/commands/abandonGame.ts
@@ -7,27 +7,22 @@ export default new Command({
 	description: "Abandon a game due to regulations",
 	options: [
 		{
-			name: "game-id",
+			name: "game-reference",
 			description: "The ID of the game to abandon/cancel",
 			type: "STRING",
 			required: true,
 		},
 	],
 
-	/* 
-	1. Get the game from the GameID
-	2. Make sure everyone confirms they want to abandon the game
-	3. Send a message to the channel that the game is being abandoned
-	4. Delete the RankedGame from the database
-	*/
-
 	run: async ({ interaction }) => {
 		//* Message conf type guard
 		if (!interaction.inCachedGuild()) return;
 
-		const gameRefOption = interaction.options.getString("game-id");
+		const gameRefOption = interaction.options.getString("game-reference");
 
-		const gameExists = await RankedGame.findOne({ gameID: gameRefOption });
+		const gameExists = await RankedGame.findOne({ gameRef: gameRefOption });
+
+		console.log(gameExists);
 
 		if (!gameExists) {
 			interaction.reply("That game doesn't exist! Please try again.");
@@ -41,7 +36,7 @@ export default new Command({
 
 		const actuallyAbandonMethod = () => {
 			gameExists.remove();
-			interaction.reply(
+			interaction.followUp(
 				`The game on ${gameExists.gameMap} with the ID ${gameExists.gameRef} has been abandoned!`
 			);
 		};
@@ -52,8 +47,6 @@ export default new Command({
 		const allGamePlayerIDs = allGamePlayerMentions.map((player) =>
 			player.replace(/<@!?(\d+)>/, "$1")
 		);
-
-		const reactedIDs = [];
 
 		const message = await interaction.reply({
 			content: `Can the following people please confirm that they want to abandon the game:\n${allGamePlayerMentions.join(
@@ -87,7 +80,7 @@ export default new Command({
 
 			if (usersReacted.length === allGamePlayerIDs.length) {
 				collector.stop();
-				//* makeTeamsDoFinal(players);
+				actuallyAbandonMethod();
 			}
 		});
 
